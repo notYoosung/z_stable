2e047aeb2a6b07be12f379147a0c92d4d0e00f2e
notYoosung <notYoosung4901@gmail.com>
2024-09-24 23:11:10 -0500

    asliases

--- z_aliases/init.lua
+++ z_aliases/init.lua
@@ -5,9 +5,9 @@ dofile(modpath .. "/mcl_aliases.lua")
 
 minetest.register_on_mods_loaded(function()
     for k, v in pairs(minetest.registered_items) do
-        minetest.log(tostring(k))
+        minetest.log(tostring(string.match(v.description or "", "[^\n]+")) .. "," .. tostring(k))
     end
     for k, v in pairs(minetest.registered_entities) do
-        minetest.log(tostring(k))
+        minetest.log(tostring(string.match(v.description or "", "[^\n]+")) .. "," .. tostring(k))
     end
 end)
