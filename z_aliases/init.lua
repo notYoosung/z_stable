df9dfd74b025ba9bac57ef3305e5296def46df3e
notYoosung <notYoosung4901@gmail.com>
2024-09-24 20:22:17 -0500

    aliases

--- z_aliases/init.lua
+++ z_aliases/init.lua
@@ -7,9 +7,6 @@ minetest.register_on_mods_loaded(function()
     for k, v in pairs(minetest.registered_items) do
         minetest.log(tostring(k))
     end
-    for k, v in pairs(minetest.registered_nodes) do
-        minetest.log(tostring(k))
-    end
     for k, v in pairs(minetest.registered_entities) do
         minetest.log(tostring(k))
     end
